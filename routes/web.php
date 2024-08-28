84d9cef7c26751d2c77812d0558d0e9d36d71272
Ian <ian.escarro@gmail.com>
2024-08-28 10:15:13 +0800

    add test hello

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 use App\Http\Controllers\ItemsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test/hello', [TestController::class, 'hello']);
 Route::resource('items', ItemsController::class);
