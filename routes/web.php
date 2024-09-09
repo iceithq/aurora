b8981ce938e63bb92cff5878461252ac1e1c339f
Ian <ian.escarro@gmail.com>
2024-09-09 10:47:56 +0800

    init breeze

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\ItemsController;
-use App\Http\Controllers\PatientsController;
-use App\Http\Controllers\HomeController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/login', function () {
-    return 'Login';
-})->name('login');
-Route::get('/test/hello', [TestController::class, 'hello']);
-Route::resource('items', ItemsController::class);
-Route::resource('patients', PatientsController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
