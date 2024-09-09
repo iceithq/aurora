592058eaa4f21b90214533ae860e62b61e994676
Ian <ian.escarro@gmail.com>
2024-09-09 10:40:15 +0800

    add info for no items

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -4,27 +4,31 @@
     <p>
         <a href="{{ route('items.create') }}" class="btn btn-outline-primary ">Create item</a>
     </p>
-    <table class="table table-hover">
-        <thead>
-            <th>Item</th>
-            <th>Price</th>
-            <th></th>
-        </thead>
-        <tbody>
-            @foreach ($items as $item)
-                <tr>
-                    <td>
-                        <a href="{{ route('items.show', $item->id) }}">
-                            {{ $item->name }}
-                        </a><br>
-                        <small>{{ $item->description }}</small>
-                    </td>
-                    <td>{{ $item->price }}</td>
-                    <td>
-                        <a href="{{ route('items.edit', $item->id) }}">Edit</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @if ($items->isEmpty())
+        <p>No items available.</p>
+    @else
+        <table class="table table-hover">
+            <thead>
+                <th>Item</th>
+                <th>Price</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                    <tr>
+                        <td>
+                            <a href="{{ route('items.show', $item->id) }}">
+                                {{ $item->name }}
+                            </a><br>
+                            <small>{{ $item->description }}</small>
+                        </td>
+                        <td>{{ $item->price }}</td>
+                        <td>
+                            <a href="{{ route('items.edit', $item->id) }}">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 @endsection
